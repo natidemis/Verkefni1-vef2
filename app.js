9a65f5443fdc5ccca841e80b18b44a183fe96ac0
natidemis <natidemisket@gmail.com>
2021-02-02 09:36:16 +0000

    videos og app.js ad mestu klart

---
+++ app.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+const videos = require('./src/videos.js');
+
+const host = '127.0.0.1';
+const port = 3000;
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+
+
+app.locals.parseDate = (d) => {
+    const date = new Date(d);
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year  = date.getFullYear();
+    return `${day}.${month}.${year}`;
+}
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/videos',express.static(path.join(__dirname,'public/videos')));
+app.use('/',videos);
+
+function notFoundHandler(req,res,next){
+    const message = "Ó nei, efni finnst ekki";
+    const title ="Fannst Ekki";
+    res.status(404).render('error',{title, message});
+
+}
+
+function errorHandler(err,req,res,next){
+    console.error(err);
+    const title = 'Villa kom upp';
+    const message = 'Ekkert fannst';
+    res.status(500).render('error',{title,message})
+}
+
+app.use(errorHandler);
+app.use(notFoundHandler);
+
+app.listen(port, host, () => {
+  console.info(
+    `Server @ http://${host}:${port}/`,
+  );
+});
---
+++ src/videos.js
@@ -0,0 +1,71 @@
+const express= require('express');
+const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const router = express.Router();
+
+
+/**
+ * Higher-order fall sem umlykur async middleware með villumeðhöndlun.
+ *
+ * @param {function} fn Middleware sem grípa á villur fyrir
+ * @returns {function} Middleware með villumeðhöndlun
+ */
+function catchErrors(fn) {
+    return (req, res, next) => fn(req, res, next).catch(next);
+  }
+
+async function fetchVideos(){
+    const file = await readFileAsync('./videos.json');
+    const json = JSON.parse(file);
+    return json;
+}
+
+async function videoList(req,res){
+    const title = "Myndbönd";
+    const json = await fetchVideos();
+    const mapped = [];
+    json.categories.forEach((item,index) => {
+        items = []
+        item.videos.forEach((val,idx) =>{
+            var obj;
+            json.videos.forEach((vid,i) =>{
+                if(val == vid.id){
+                    obj = vid;
+                }
+            })
+            items.push(obj);
+        })
+        var result;
+        var result = {
+            title:item.title,
+            videos:items,
+        }
+        mapped.push(result)
+    } )
+    res.render('index',{title,mapped});
+}
+
+async function videos(req,res,next){
+    const {id} = req.params;
+
+    const json = await fetchVideos();
+    const {videos } = json;
+    const foundVideos = videos.find(a => a.id === parseInt(id));
+
+    if(!foundVideos){
+        return next();
+    }
+
+    const {title} = foundVideos;
+    console.log({title, content: foundVideos})
+    return res.render('index',{title, content: foundVideos})
+}
+
+
+router.get('/',catchErrors(videoList));
+router.get('/:id',catchErrors(videos));
+
+module.exports = router;
+
+
